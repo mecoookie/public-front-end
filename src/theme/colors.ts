a3eaa229d88a22cc6be6cc6131bae9b7d6d3d0cb
mecoookie <mecoookie@protonmail.com>
2021-10-04 03:52:02 -0400

    color nuke

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,3 +1,26 @@
+// export const white = '#FFF'
+// export const black = '#000'
+
+// export const green = {
+//   500: '##00d1810',
+// }
+
+// export const red = {
+//   100: '#FFFDFE',
+//   200: '#ffc2a8',
+//   500: '#e36f10',
+// }
+
+// export const grey = {
+//   100: '#f7f4f2',
+//   200: '#f0e9e7',
+//   300: '#cf8548',
+//   400: '#e36f10',
+//   500: '#ff943d',
+//   600: '#cf6611',
+//   800: '#8a3e00',
+// }
+
 export const white = '#FFF'
 export const black = '#000'
 
@@ -6,19 +29,19 @@ export const green = {
 }
 
 export const red = {
-  100: '#FFFDFE',
-  200: '#ffc2a8',
-  500: '#e36f10',
+  100: '#FF0000',
+  200: '#FF0000',
+  500: '#FF0000',
 }
 
 export const grey = {
-  100: '#f7f4f2',
-  200: '#f0e9e7',
-  300: '#cf8548',
-  400: '#e36f10',
-  500: '#ff943d',
-  600: '#cf6611',
-  800: '#8a3e00',
+  100: '#00FF00',
+  200: '#00FF00',
+  300: '#00FF00',
+  400: '#00FF00',
+  500: '#0000FF',
+  600: '#0000FF',
+  800: '#0000FF',
 }
 
 export const marbicolor = {
